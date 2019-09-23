d044d9ef5a44b623cf5ff7bebdca0ce2ec28c264
TinsonWang <tinsonwang@gmail.com>
2019-09-23 19:26:32 -0400

    Second challenge

--- rle.py
+++ rle.py
@@ -8,15 +8,23 @@ def main(args):
 
 	#dude im so sorry i dont really understand this challenge, but i did stuff for 'operating modes' section 
 
-
-
-
     if argv[1] == "c":		#compress file at argv[2]
 
     	with open (argv[2], "r") as f:		
 
-    		for line in f:		#prints each line of file
-    			print (line)
+    		for line in f:					#prints each line of file
+				memory = ""					#Set a variable to remember letters
+				counter = 0					#Set a variable to count how many letters
+    			for letter in line:			#For each character in the line
+					if memory == "":		#If memory variable is empty, 
+						memory = letter		#Store that letter
+					elif letter == memory:	#If the letter matches the one stored in memory...
+						counter += 1		#Increase counter
+					elif letter != memory:	#When a different letter is encountered...
+						print(f"{counter}{memory}") 	#Print out the # of times the previous letter was counted and what letter it was
+						memory = letter					#Set memory equal to the new letter
+					elif letter == " "					#If letter is a space
+						print(" ")						#Print a space
 
 
     if argv[1] == "x": 		#decompress file at argv[2]
@@ -24,7 +32,10 @@ def main(args):
     	with open (argv[2], "r") as f:		
 
     		for line in f:		#prints each line of file
-    			print (line)
+    			for letter in f:					#The following block is in pseudo-code
+					if letter == number:			#If the character encountered is a number
+						print(that number X letters)		#Print the following character (which should be a letter) 'number' of times
+
 
     return 0
 
